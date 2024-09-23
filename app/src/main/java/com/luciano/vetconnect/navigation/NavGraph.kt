17e4361f0e9da39d9c1795d8a0fcfcbfe005760a
LucianRuiz <luciano.software@hotmail.com>
2024-09-22 20:49:16 -0500

    feat: add Splash, Login and Register Screen

--- app/src/main/java/com/luciano/vetconnect/navigation/NavGraph.kt
+++ app/src/main/java/com/luciano/vetconnect/navigation/NavGraph.kt
@@ -1,2 +1,37 @@
 package com.luciano.vetconnect.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.luciano.vetconnect.features.auth.login.LoginScreen
+import com.luciano.vetconnect.features.auth.register.RegisterScreen
+import com.luciano.vetconnect.features.splash.SplashScreen
+
+sealed class Screen(val route: String) {
+    object Splash : Screen("splash")
+    object Login : Screen("login")
+    object Register : Screen("register")
+}
+
+@Composable
+fun NavGraph(
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = Screen.Splash.route
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(Screen.Splash.route) {
+            SplashScreen(navController = navController)
+        }
+        composable(Screen.Login.route) {
+            LoginScreen(navController = navController)
+        }
+        composable(Screen.Register.route) {
+            RegisterScreen(navController = navController)
+        }
+    }
+}
